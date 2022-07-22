679637c25bbcf067d212f54e4ac2426df962beff
Ryan Baumgardner <rbaumgardner@planview.com>
2022-07-22 16:49:37 -0400

    feat(extra): complete useReducer extra credit 2

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,14 +3,18 @@
 
 import * as React from 'react'
 
-function countReducer(count, step) {
-	return count + step;
+function countReducer(state, action) {
+	return { ...state, ...action };
 }
 
 function Counter({initialCount = 0, step = 1}) {
-  const [ count, changeCount ] = React.useReducer(countReducer, initialCount);
+  const [ state, setState ] = React.useReducer(countReducer, {
+	  count: initialCount
+  });
 
-  const increment = () => changeCount(step)
+  const { count } = state;
+
+  const increment = () => setState({ count: count + step })
   return <button onClick={increment}>{count}</button>
 }
 
