6af8467e49e42d4b34f2366716d3a926085d7f1a
Ryan Baumgardner <rbaumgardner@planview.com>
2022-08-26 16:12:13 -0400

    feat(extra): complete exercise 2 extra credit 3

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -10,6 +10,24 @@ import {
   PokemonErrorBoundary,
 } from '../pokemon'
 
+function useSafeDispatch(dispatch) {
+	const mountedRef = React.useRef(false);
+
+	React.useLayoutEffect(() => {
+		mountedRef.current = true;
+
+		return () => {
+			mountedRef.current = false;
+		}
+	}, [])
+
+	return React.useCallback( (...args) => {
+		if ( mountedRef.current ) {
+			dispatch(...args)
+		}
+	}, [dispatch])
+}
+
 function asyncReducer(state, action) {
   switch (action.type) {
     case 'pending': {
@@ -28,12 +46,14 @@ function asyncReducer(state, action) {
 }
 
 function useAsync(initialState) {
-	const [state, dispatch] = React.useReducer(asyncReducer, {
+	const [state, unsafeDispatch] = React.useReducer(asyncReducer, {
 		status: 'idle',
 		data: null,
 		error: null,
 		...initialState,
 	});
+
+	const dispatch = useSafeDispatch( unsafeDispatch );
 
 	const run = React.useCallback(promise => {
 		dispatch({type: 'pending'})
@@ -45,7 +65,7 @@ function useAsync(initialState) {
 				dispatch({type: 'rejected', error})
 			},
 		)
-	}, [])
+	}, [ dispatch ])
 
 	return {
 		...state,
